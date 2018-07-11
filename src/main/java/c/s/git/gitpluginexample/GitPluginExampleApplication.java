fd6ff6ea9d46aa76d820779557d25a545dc2a7b4
chineshine <chineshine@outlook.com>
2018-07-11 17:56:38 +0800

    init

---
+++ src/main/java/c/s/git/gitpluginexample/GitPluginExampleApplication.java
@@ -0,0 +1,12 @@
+package c.s.git.gitpluginexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPluginExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPluginExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/c/s/git/gitpluginexample/GitPluginExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package c.s.git.gitpluginexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitPluginExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
